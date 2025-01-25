bc2584bd4c33a5ed43a02208db75148d2624a352
moyeed <moyeed95@gmail.com>
2025-01-25 11:21:30 -0800

    rectified

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+/** @type {import('eslint').Linter.Config} */
+export default {
+  files: ["**/*.{js,mjs,cjs,jsx}"],  // Matches files with these extensions
+  languageOptions: {
+    globals: globals.browser,  // Use browser global variables
+  },
+  plugins: {
+    'react': pluginReact,
+    '@eslint/js': pluginJs,
+  },
+  extends: [
+    pluginJs.configs.recommended,  // Use the recommended ESLint JS rules
+    pluginReact.configs.flat.recommended,  // Use the recommended React rules
+  ],
+};
